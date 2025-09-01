4a209805f05a061ade477fc1012abcf6faddb241
vasiliy <vstrelnikov137@gmail.com>
2025-09-01 21:15:00 +0300

    again

--- main.py
+++ main.py
@@ -11,22 +11,30 @@ class Dot:
     def __str__(self):
         return f'Dot({self._x}, {self._y})'
 
+    def __hash__(self):
+        return hash((self._x, self._y))
+
 
 class Ship:
     def __init__(self, length: int, nose: Dot, drctn: int): #если drctn==0 => направление вертикальное, 1 = горизонтальное
-        self.__length = length
-        self.__nose = nose
-        self.__drctn = drctn
-        self.__life = length
+        self._length = length
+        self._nose = nose
+        self._drctn = drctn
+        self._life = length
 
     def __eq__(self, other):
-        return self.__length == other.length and self.__drctn == other.__drctn
+        return self._length == other._length and self._drctn == other._drctn
+
     @property
     def dots(self):
-        if self.__drctn == 0:
-            return [Dot(self.__nose._x, y) for y in range(self.__nose._y - self.__length + 1, self.__nose._y + 1)]
-        elif self.__drctn == 1:
-            return [Dot(x, self.__nose._y) for x in range(self.__nose._x - self.__length + 1, self.__nose._x + 1)]
+        if self._drctn == 0:
+            return [Dot(self._nose._x, y) for y in range(self._nose._y - self._length + 1, self._nose._y + 1)]
+        elif self._drctn == 1:
+            return [Dot(x, self._nose._y) for x in range(self._nose._x - self._length + 1, self._nose._x + 1)]
+
+    @property
+    def get_shipdots(self):
+        return [str(i) for i in self.dots]
 
 
 class Board:
@@ -35,31 +43,41 @@ class Board:
         self.ships = set()
         self.count_of_ships = {'1': 0, '2': 0, '3': 0}
         self.hid = True
-        self.contour = self.ships
+        # self.contour = self.ships мб без контура
     state = [Dot(x, y) for y in range(1, 7) for x in range(1, 7)]
     count_ships = 0
 
+    @property
+    def get_shipsdots(self):
+        return [str(i) for i in self.ships]
+
     def add_ship(self, ship: Ship):
         flag = True
+        if ship._length == 3 and self.count_of_ships['3'] > 0:
+            raise ValueError
+        #доделать условие и потом делать метод shot
         for dot in ship.dots:
-            if dot not in Board.state:
+            if (dot not in Board.state) or (dot in self.ships):
                 flag = False
-            if flag:
-                for dot in ship.dots:
-                    self.visual[dot._y-1][dot._x-1] = '■'
-                    self.ships.add(dot)
-                    for x in range(dot._x-1, dot._x+2):
-                        for y in range(dot._y-1, dot._y+2):
-                            self.contour.add(Dot(x, y))
-            else:
-                raise ValueError
+        if flag:
+            for dot in ship.dots:
+                self.visual[dot._y-1][dot._x-1] = '■'
+                # self.ships.add(dot) мб без контура
+                for x in range(dot._x-1, dot._x+2):
+                    for y in range(dot._y-1, dot._y+2):
+                        self.ships.add(Dot(x, y))
+        else:
+            raise ValueError
 
 class Player:
     pass
 
-ship1 = Ship(3, Dot(4, 6), 0)
+ship1 = Ship(3, Dot(6, 6), 0)
+ship2 = Ship(3, Dot(6, 2), 1)
 pl1_board = Board(True)
-# pl1_board.add_ship(ship1)
+pl1_board.add_ship(ship1)
+pl1_board.add_ship(ship2)
 print(*pl1_board.visual, sep='\n')
+
 
 
